2622d4fc7238c383f1795bc63b6d22adde34a157
santosh nanduri <32795188+santosh0305@users.noreply.github.com>
2020-09-20 08:33:13 +0530

    Clean up Singleton

---
+++ 5DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+
+            Thread.Sleep(3000);
+
+            var newDelhi = db.GetPopulation("New Delhi");
+            System.Console.WriteLine(newDelhi);
+
+            var wachingtonDc = db2.GetPopulation("Washington, D.C.");
+            System.Console.WriteLine(wachingtonDc);
+
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ 5DesignPatterns/SingletonPattern/SingletonDataContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingletonPattern
+{
+    public class SingletonDataContainer
+    {
+        private readonly Dictionary<string, int> capitals = new Dictionary<string, int>();
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing Singleton Object");
+
+            capitals.Add("New Delhi", 16787949);
+            capitals.Add("Washington, D.C.", 658893);
+            capitals.Add("London", 8630100);
+            capitals.Add("Beijing", 20693000);
+        }
+        
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+}
